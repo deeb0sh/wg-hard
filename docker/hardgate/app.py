c12674b5683e9c29fef4b1f4308cd3468f29fd66
deeb0sh <deeb0sh@ya.ru>
2024-06-22 11:58:19 +0300

    remove

---
+++ docker/hardgate/app.py
@@ -0,0 +1,188 @@
+import os
+from flask import Flask, jsonify, request, json
+from flask_cors import CORS
+from dotenv import load_dotenv
+import psycopg2
+import jwt
+import datetime
+import re
+
+app = Flask(__name__)
+CORS(app)
+
+# .env
+# KEY=''
+# DATABASE=''
+load_dotenv()
+url_db = os.getenv("DATABASE")
+app.config['secret_key'] = os.getenv("KEY")
+
+try:
+    conn = psycopg2.connect(url_db)
+    c = conn.cursor()
+except:
+        print('NO CONNECT DATABASE')
+
+def select(table,login):
+    c.execute(f"""
+             SELECT "clients" FROM {table} WHERE id_acc = (SELECT id_acc FROM accounts WHERE login = ('{login}'))
+        """)
+    tmp=c.fetchone()
+    x = json.dumps(tmp)
+    x = json.loads(x)
+    for i in x[0]:
+        del i["pubkey"],i["privkey"]
+    return x[0]
+    
+def select_serv(table,login):
+    c.execute(f"""
+          select jsonb_build_object('w_host', server->'w_host','clients', '') from {table} WHERE id_acc = (SELECT id_acc FROM accounts WHERE login = ('{login}'))
+          """)
+    tmp=c.fetchone()
+    x = json.dumps(tmp)
+    x = json.loads(x)
+    return x[0]
+
+def select_s_conf(table,login):
+    c.execute(f"""
+          select jsonb_build_object('w_host', server->'w_host','pubkey', server->'pubkey','w_port', server->'w_port') from {table} WHERE id_acc = (SELECT id_acc FROM accounts WHERE login = ('{login}'))
+          """)
+    tmp=c.fetchone()
+    x = json.dumps(tmp)
+    x = json.loads(x)
+    return x[0]
+
+def select_c_conf(table,login,user):
+    c.execute(f"""
+             SELECT "clients" FROM {table} WHERE id_acc = (SELECT id_acc FROM accounts WHERE login = ('{login}'))
+        """)
+    tmp=c.fetchone()
+    x = json.dumps(tmp)
+    x = json.loads(x)
+    return x[0]
+
+def token_required(f):
+    def decorated(*args, **kwargs):
+        #token = request.headers.get('token')
+        #token = request.args.get('token')
+        token = request.cookies.get('t')
+        if not token:
+            return jsonify({'error': 'NO ACCESS'}), 403
+        try:
+            jwt.decode(token, app.config['secret_key'], algorithms="HS256")
+        except Exception as error:
+            return jsonify({'error': 'BAD TOKEN'}),403
+        return f(*args, **kwargs)
+    return decorated
+
+
+# @app.route('/api', methods=['GET'])
+# def cookies_test():
+#     token = request.cookies.get('t')
+#     if token:
+#         data = jwt.decode(token, app.config['secret_key'], algorithms="HS256")
+#         response = jsonify({ 
+#             "name": data['user'],          
+#             "auth": True
+#             })
+#         # response.set_cookie("t", token, max_age=0)
+#         # token = jwt.encode({
+#         #             'user': data['user'] , 
+#         #             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=86400)
+#         #             }, app.config['secret_key'])
+#         # response.set_cookie("t", value=token, secure=False, httponly=True, max_age=86400)
+#         return response
+#     else:
+#         return jsonify({
+#             "name": "no auth",
+#             "auth": False
+#         })
+
+
+@app.route('/api/login',methods=['POST','OPTIONS'])
+def login():
+    data = request.get_json()
+    name = data["usr"]
+    password = data["passwd"]
+    CHECK_ACC = f"SELECT (login = '{name}') AND (password = crypt('{password}', password)) FROM accounts;"
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(CHECK_ACC)
+            results = cursor.fetchall()
+            res = re.search(r"True",str(results))
+            if res:
+                token = jwt.encode({
+                    'user': name , 
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=86400)
+                    }, app.config['secret_key'])
+                response = jsonify({
+                    "auth": True,
+                    "t": token
+                    })
+                # response.set_cookie("t", value=token, secure=False, httponly=True, max_age=86400)
+                return response               
+            else:
+                return jsonify({
+                    "auth": False,
+                    })
+
+ 
+@app.route("/api/home",endpoint="home" , methods=['GET'])
+@token_required
+def home():
+    # тут личный кабинет и параментры для конфигов клиентов
+    token = request.cookies.get('t') 
+    data = jwt.decode(token, app.config['secret_key'], algorithms=['HS256'])
+    x=select_serv("server_fi",data["user"])
+    x["clients"] = select("server_fi",data["user"])
+    y=select_serv("server_ru",data["user"])
+    y["clients"] = select("server_ru",data["user"])
+    return [x,y]
+
+@app.route("/api/getconf/<string:server>/<string:user>", endpoint="getconf", methods=['GET'])
+@token_required
+def getWGconf(server,user):
+    if server == "ru.darksurf.ru":
+        server = "server_ru" 
+    elif server == "fi.darksurf.ru":
+        server = "server_fi"
+
+    token = request.cookies.get('t') 
+    data = jwt.decode(token, app.config['secret_key'], algorithms=['HS256'])
+    x = select_s_conf(server,data["user"])
+    y = select_c_conf(server,data["user"],user)
+    for i in y:
+        if i["name"] == user:
+                wg_conf = (f"""
+### DarkSurf.ru v0.1
+### user:{user} 
+### server:{server}
+                          
+[Interface]
+PrivateKey = {i["privkey"]}
+Address = {i["ip"]}/24
+DNS = 45.142.122.244,77.221.159.104 
+
+[Peer]
+PublicKey = {x["pubkey"]}
+AllowedIPs = 0.0.0.0/0
+Endpoint = {x["w_host"]}:{x["w_port"]}""") 
+            
+    print(wg_conf)
+    return wg_conf
+
+# @app.route("/api/logout",endpoint="logout", methods=['GET'])
+# @token_required
+# def logout():
+#     token = request.cookies.get('t')
+#     response = jsonify({ 
+#             "msg": "ok"         
+#             })
+#     response.set_cookie("t", token, max_age=0)
+#     return response    
+    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ docker/hardunit/app/server.py
@@ -0,0 +1,90 @@
+import os
+import psycopg2
+from flask import Flask, jsonify, json
+import ipaddress
+from waitress import serve
+import subprocess
+
+
+app = Flask(__name__)
+#CORS(app)
+
+def select(rows,table,login):
+    c.execute(f"""
+          SELECT {rows} FROM {table} WHERE id_acc = (SELECT id_acc FROM accounts WHERE login = ('{login}'))
+          """)
+    tmp=c.fetchone()
+    x = json.dumps(tmp)
+    x = json.loads(x)
+    return x[0]
+
+def update(rows,table,json,login):
+    c.execute(f"""
+          UPDATE {table} SET {rows} = '{json}'::jsonb WHERE id_acc = (SELECT id_acc FROM accounts WHERE login = ('{login}'))
+          """)
+    conn.commit()
+    return 0
+
+def config():
+    wg0_conf = (f"""### DarkSurf.ru (c)
+[Interface]
+PrivateKey = {s_privkey}
+Address = {wg_gateway[1]}/24
+ListenPort = 51820
+PostUp = iptables -t nat -A POSTROUTING -s {wg_lan} -o eth0 -j MASQUERADE; iptables -P INPUT ACCEPT; iptables -P OUTPUT ACCEPT; iptables -P FORWARD ACCEPT""")
+    for c_conf in select("clients",table,login):
+        wg0_conf = wg0_conf + (f'\n\n[Peer] # Client = {c_conf["name"]}\nPublicKey = {c_conf["pubkey"]}\nAllowedIPs = {c_conf["ip"]}/32')
+
+    with open("/etc/wireguard/wg0.conf","w") as file:
+        file.write(wg0_conf) # ЧИИИК!!
+    return 0
+
+# ifconfig eth0 | grep inet | awk -F: '{print $2}'| awk '{print $1}') 
+ip = subprocess.run(["/app/c_ip.sh"],capture_output=True, text=True) # чииииик !!!
+c_ip = ip.stdout
+login = os.environ.get("LOGIN")
+w_port = os.environ.get("W_PORT")
+w_host = os.environ.get("W_HOST")
+url_db = os.environ.get("DATABASE")
+wg_lan = os.environ.get("WG_LAN")
+table = os.environ.get("TABLE")
+
+# url_db='postgresql://user:password@host:port/database_name'
+
+try:
+    conn = psycopg2.connect(url_db)
+except:
+    print('NO CONNECT DATABASE')
+
+c = conn.cursor()
+s_conf = select("server",table,login)
+s_conf["c_ip"] = c_ip.strip() # перемменная.strip убрать \n
+s_conf["w_port"] = w_port
+s_conf["w_host"] = w_host
+s_conf["wg_lan"] = wg_lan
+s_privkey = s_conf["privkey"]
+s_pubkey = s_conf["pubkey"]
+s_tc = s_conf["tc"]
+s_conf = json.dumps(s_conf)
+wg_gateway = ipaddress.ip_network(wg_lan) # чиик !!!
+update("server",table,s_conf,login) # ХОП !
+
+config() # ЧИИИИИИИИИИИИИИИИККККК!
+
+os.chmod("/etc/wireguard/wg0.conf",0o600) # чик
+subprocess.run(["wg-quick", "up", "wg0"]) # ОПА!!!!!!!
+
+###### 
+
+
+@app.route('/punch')
+def punch():
+    config()
+    subprocess.run(["/app/wg_reload.sh"]) # wg syncconf wg0 <(wg-quick strip wg0) перечитать конфиг
+    return jsonify({"msg":"ok"})
+
+
+if __name__ == "__main__":
+  serve(app, host='0.0.0.0', port=5000)
+  #app.run(debug=True, host='0.0.0.0', port=5000) # dev mode
+   
