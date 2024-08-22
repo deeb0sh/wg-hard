7ea85281896b8f7723f97e77f108a6c16d4fdba8
deeb0sh <deeb0sh@ya.ru>
2024-08-22 20:17:23 +0300

    пришит рука к пизде

--- docker/hardunit/app/wg_stats.sh
+++ docker/hardunit/app/wg_stats.sh
@@ -7,3 +7,4 @@ while read -r out; do
       ((n >= i )) &&  echo $out | (IFS='         ' read -r x1 x2 x3 x4 x5 x6 x7 x8 x9 x10; echo -n '{"last": '$x5', "tx": '$x6', "rx": '$x7'},')
       ((n++))
 done
+echo -n '{"last": 0, "tx": 0, "rx": 0}' # пришит рукав к пизде
