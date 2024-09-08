14a70c3a07f1b26ce9aad749bf4fd21d9f8a8f6e
deeb0sh <deeb0sh@ya.ru>
2024-09-08 19:20:55 +0300

    +ss wg

--- docker/hardunit_ss/app/server.py
+++ docker/hardunit_ss/app/server.py
@@ -73,7 +73,7 @@ config() # ЧИИИИИИИИИИИИИИИИККККК!
 
 os.chmod("/etc/wireguard/wg0.conf",0o600) # чик
 subprocess.run(["wg-quick", "up", "wg0"]) # ОПА!!!!!!!
-subprocess.run(["ssserver","-U","--server-addr","0.0.0.0:8388","--encrypt-method","aes-256-gcm","--password","qweqwe1"])
+subprocess.run(["ssserver","-c","/app/config.json","-d"]) # запуск сервера shadowsocks-rust
 
 ###### 
 
